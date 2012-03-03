c93744230445f1d0f42e147d28f1a33c32f33e07
Javier <javiermon@gmail.com>
2012-03-03 21:12:59 +0100

    and a bunch of more links

--- handy.py
+++ handy.py
@@ -10,5 +10,17 @@ handy_links = {
     'trollface'  : 'http://i.imgur.com/Njy8l.png',
     'sfuck'      : 'http://i.imgur.com/sXtZc.jpg',
     'goback'     : 'http://i.imgur.com/pLQUU.gif',
-    'dealwithit' : 'http://i.imgur.com/G7f9J.gif'
+    'dealwithit' : 'http://i.imgur.com/G7f9J.gif',
+    'likeigive'  : 'http://i.imgur.com/6kO74.gif',
+    'mindblown'  : 'http://i.imgur.com/fbelJ.gif',
+    'clap'       : 'http://i.imgur.com/1WypN.gif',
+    'truestory'  : 'http://i.imgur.com/51Ekh.png',
+    'laugh'      : 'http://i.imgur.com/JFbhu.gif',
+    'rolleyes'   : 'http://i.imgur.com/RqtZe.gif',
+    'fuckthis'   : 'http://i.imgur.com/Bsfnw.gif',
+    'lol'        : 'http://i.imgur.com/LFguu.gif',
+    'wrong'      : 'http://i.imgur.com/TBL3j.gif',
+    'coolstory'  : 'http://i.imgur.com/iVLKt.gif',
+    'f7u12'      : 'http://i.imgur.com/U8jcB.gif',
+    'facepalm'   : 'http://i.imgur.com/6eKQz.gif'
 }
